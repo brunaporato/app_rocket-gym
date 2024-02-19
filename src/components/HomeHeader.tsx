5157705e43555440c7f8494e9bace61d001b91e7
Bruna <brunaporato@gmail.com>
2024-02-19 07:44:34 -0300

    add headerhome component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { VStack, Heading, Text, HStack, Icon } from '@gluestack-ui/themed'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import { UserImage } from './UserImage'
+import { TouchableOpacity } from 'react-native'
+
+export function HomeHeader() {
+  return (
+    <HStack bgColor="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+      <UserImage
+        source={{ uri: 'https://github.com/brunaporato.png' }}
+        size={64}
+        mr={16}
+      />
+      <VStack flex={1}>
+        <Text color="$gray100" fontSize="$md">
+          Olá,
+        </Text>
+        <Heading color="$gray100" fontSize="$md">
+          Bruna
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} color="$gray200" size="xl" name="logout" />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
---
+++ src/components/UserImage.tsx
@@ -0,0 +1,22 @@
+import { Image } from '@gluestack-ui/themed'
+import { ImageProps } from 'react-native'
+
+type UserImageProps = ImageProps & {
+  size: number
+  mr?: number
+}
+
+export function UserImage({ size, mr, ...props }: UserImageProps) {
+  return (
+    <Image
+      w={size}
+      h={size}
+      rounded="$full"
+      borderWidth={2}
+      borderColor="$gray400"
+      alt="User's image"
+      mr={mr}
+      {...props}
+    />
+  )
+}
