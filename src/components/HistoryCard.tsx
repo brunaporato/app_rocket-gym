3771a3b83f2088d10dd3b5799d8ea28938b4011e
Bruna <brunaporato@gmail.com>
2024-02-20 06:18:26 -0300

    add section list at history screen

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,29 @@
+import { HStack, Heading, Text, VStack } from '@gluestack-ui/themed'
+
+export function HistoryCard() {
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      mb="$3"
+      bgColor="$gray600"
+      rounded="$md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr="$5">
+        <Heading color="$white" fontSize="$md" textTransform="capitalize">
+          Costas
+        </Heading>
+        <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+
+      <Text color="$gray300" fontSize="$md">
+        08:30
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,28 @@
+import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from '@gluestack-ui/themed'
+import { Heading, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { SectionList } from 'react-native'
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    { title: '26.08.24', data: ['Puxada frontal', 'remada unilateral'] },
+    { title: '27.08.24', data: ['Puxada lateral'] },
+  ])
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de Exercícios" />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => (
+          <Heading color="$gray200" fontSize="$md" mt="$8" mb="$3">
+            {section.title}
+          </Heading>
+        )}
+        style={{ paddingHorizontal: 32 }}
+      />
     </VStack>
   )
 }
