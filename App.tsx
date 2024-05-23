a4415510f49c90a3185af4b73b4969eab42753c3
brunaporato <brunaporato@gmail.com>
2024-05-23 07:54:30 -0300

    config onesignal

--- App.tsx
+++ App.tsx
@@ -10,6 +10,10 @@ import { Loading } from '@components/Loading'
 import { config } from 'config/gluestack-ui.config'
 import { Routes } from './src/routes'
 import { AuthContextProvider } from '@contexts/AuthContext'
+import { OneSignal } from 'react-native-onesignal'
+
+OneSignal.initialize('d04ee8a7-9ee9-4723-950c-97499740bdc9')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({
